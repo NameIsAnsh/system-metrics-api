8486c3bc2f2412930fbfe069a1516dd6c3c5bd2b
Ansh <anshkrsna@gmail.com>
2026-05-24 13:57:24 +0530

    initial setup with fastapi ,psutil and docker container

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+import psutil 
+app=FastAPI()
+
+
+@app.get("/cpu")
+def get_cpu_usage():
+    cpu_usage = psutil.cpu_percent(interval=1)
+    return {"cpu_percentage": cpu_usage}
+@app.get("/ram")
+def get_ram_usage():
+    ram_usage= psutil.virtual_memory().percent
+    return {"ram_percent" : ram_usage}
+

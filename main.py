25a97fc03f0f45428b60852f48a0423954527694
Ansh <anshkrsna@gmail.com>
2026-05-25 15:43:17 +0530

    setup github actions pipeline

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 import psutil 
 app=FastAPI()
 
-
+@app.get("/health")
+def health_check():
+    return {"status":"healthy","version":"1.1 - CI/CD Active"}
 @app.get("/cpu")
 def get_cpu_usage():
     cpu_usage = psutil.cpu_percent(interval=1)
